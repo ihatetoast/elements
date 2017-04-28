6c50f5a702b86fe029dffe825a8faf860e3d7593
Katy <ihatetoast@gmail.com>
2017-04-27 19:14:56 -0500

    added fcn so cards load as I build. will del once stack built or flashcard quiz section done

--- main.js
+++ main.js
@@ -1,40 +1,85 @@
 $(document ).ready(function() {
 	$('#toggleForm').on('click', () => {
-		console.log('toggle form button clicked;');
 		$('#elemForm').slideToggle('slow');
-	})
+	});
 
-// http://small-tiyfe.herokuapp.com/collections/katyelements
+	$(function(){
+		//variables for string literal
+		const elemStack = $('#elemStack');
+		const name = $('#elemEntry');
+		const symbol = $('#elemSymbol');
+		const number = $('#atomNoEntry');
+		const deutsch = $('#deutsch');
+		const trivia = $('#trivia');
 
-  //variables for string literal
-	const name = $('#elemEntry');
-	const symbol = $('#elemSymbol');
-	const number = $('#atomNoEntry');
-	const deutsch = $('#deutsch');
-	const trivia = $('#trivia');
-// const saveBtn = document.getElementById('saveBtn');
+		function addElement(element){
+			elemStack.prepend(
+				`<div class="flexColumn element">
+					<p class="atomicNo">${element.number}</p>
+					<p class="atomicSymbol">${element.symbol}</p>
+					<p class="elemName">${element.name}</p>
+				</div>`
+			);
+		}
+	$.ajax({
+		type: 'GET',
+		url: 'http://small-tiyfe.herokuapp.com/collections/chemicalelements/',
+		success: function(elements){
+			$.each(elements, (i, element) => {
+				addElement(element);
+			})
+		},
+		error: function(error){
+			// console.log(error);
+			alert('Error loading elements.')
+		},
+		complete: function(){
+			console.log('You gots elements.')
+		}
+	});
 	$('#saveBtn').on('click', (e)=>{
 		e.preventDefault();
 		const element ={
-			_id: `${number.val()}-${Date.now()}`,
-			name: name.val(),
-			symbol: symbol.val(),
+			name: name.val().toLowerCase(),
+			symbol: symbol.val().toLowerCase(),
 			number: number.val(),
-			deutsch: deutsch.val(),
+			deutsch: deutsch.val().toLowerCase(),
 			trivia: trivia.val()
 		}
+		console.log(element);
 		$.ajax({
 				type: 'POST',
-				url: 'http://small-tiyfe.herokuapp.com/collections/katyelements/',
+				url: 'http://small-tiyfe.herokuapp.com/collections/chemicalelements',
 				data: element,
 				success: function(newElement){
-					// addElement(newElement);
+					addElement(newElement);
 					name.val('');
 					symbol.val('');
 					number.val('');
 					deutsch.val('');
-					trivia.val('');}
+					trivia.val('');
+				},
+				error: function(error){
+					console.log(error);
+					alert("error in loading new element.");
+				},
+				complete: function(){
+					console.log('you did something fantastic.')
+				}
   
 		});
 	});
+
+	})
+  
+
+			
+// ecmasript6 for when i want to cap first letter of the chem symbol:
+// const capitalize = str => str.length
+//   ? str[0].toUpperCase() +
+//     str.slice(1).toLowerCase()
+//   : '';
+
+// const saveBtn = document.getElementById('saveBtn');
+	
 });
